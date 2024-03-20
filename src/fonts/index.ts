6ecb153fbc04c5b21f3ddbed3cfa7012e3750de8
hakko <47938592+dividenconquer@users.noreply.github.com>
2024-03-21 04:23:09 +0900

    update

---
+++ src/fonts/index.ts
@@ -0,0 +1,26 @@
+export const FONTS = [
+    {
+        family: "ABCFavoritVariableEdu",
+        url: `ABCFavoritVariableEdu-Regular.ttf`,
+    },
+    {
+        family: "ABCProphetVariableEdu",
+        url: `ABCProphetVariableEdu-Regular.ttf`,
+    },
+    {
+        family: "ABCWhyteInktrapVariableEdu",
+        url: `ABCWhyteInktrapVariableEdu-Regular.ttf`,
+    },
+    {
+        family: "BigBlueTermPlusNerdFontMono",
+        url: `BigBlueTermPlusNerdFontMono-Regular.ttf`,
+    },
+    {
+        family: "GohuFontuni11NerdFontMono",
+        url: `GohuFontuni11NerdFontMono-Regular.ttf`,
+    },
+    {
+        family: "LibreBaskerville",
+        url: `LibreBaskerville-Regular.ttf`,
+    },
+]
