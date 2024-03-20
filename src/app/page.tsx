21a999a73df7d6d84875f5796637e2398ddeafb9
hakko <47938592+dividenconquer@users.noreply.github.com>
2024-03-21 05:15:04 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ let startTime: number;
 const delay = 200;
 
 export default function Home() {
-  const [text, setText] = useState("oxfont");
+  const [text, setText] = useState("0xtype");
   const [assetLoaded, setAssetLoaded] = useState(false);
   const [fontIndex, setFontIndex] = useState(
     new Array(text.length)
@@ -111,7 +111,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute before:-z-20 after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div>
         {assetLoaded && (
           <span
             ref={textRef}
